b46fbcf9436e0a6b933e36cc510f480c8adc92a7
Kingdon Barrett <kingdon.b@nd.edu>
2019-06-21 20:16:37 -0600

    TempCluster object can tell you when it's expired
    With LocalTime, the clusters expire at midnight in America/New_York time
    zone, hopefully this can be less surprising, since they will be deleted.

--- app/models/temp_cluster.rb
+++ app/models/temp_cluster.rb
@@ -1,2 +1,12 @@
 class TempCluster < ApplicationRecord
+  EXPIRY_DAYS = 4
+  def expired?
+    expiry_date <= LocalTime.current_date
+  end
+  def expiry_date
+    created_date + EXPIRY_DAYS.days
+  end
+  def created_date
+    created_at.to_date
+  end
 end
--- spec/models/temp_cluster_spec.rb
+++ spec/models/temp_cluster_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe TempCluster, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#expired?' do
+    let(:date) { double(:expiry_date) }
+    let(:current_date) { double(:current) }
+    it 'true' do
+      expect(subject).to receive(:expiry_date).and_return date
+      expect(date).to receive(:<=).with(current_date).and_return true
+      expect(LocalTime).to receive(:current_date).and_return current_date
+      expect(subject.expired?).to eq true
+    end
+    it 'false' do
+      expect(subject).to receive(:expiry_date).and_return date
+      expect(date).to receive(:<=).with(current_date).and_return false
+      expect(LocalTime).to receive(:current_date).and_return current_date
+      expect(subject.expired?).to be_falsey
+    end
+  end
+  describe '#expiry_date' do
+    let(:created) { double('created_date') }
+    let(:d) { double('return') }
+    it '' do
+      expect(subject).to receive(:created_date).and_return created
+      expect(created).to receive(:+).with(TempCluster::EXPIRY_DAYS.days)
+        .and_return d
+      expect(subject.expiry_date).to eq d
+    end
+  end
+  describe '#created_date' do
+    let(:created) { double('created_at') }
+    let(:d) { double('return') }
+    it 'converts created_at to a date' do
+      expect(subject).to receive(:created_at).and_return created
+      expect(created).to receive(:to_date).and_return d
+      expect(subject.created_date).to eq d
+    end
+  end
 end
