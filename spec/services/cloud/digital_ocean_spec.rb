a67700e76e254f2e77a50448f975fef073760045
Kingdon Barrett <kingdon.b@nd.edu>
2019-06-21 19:46:58 -0600

    Cloud::DigitalOcean basic client

--- spec/services/cloud/digital_ocean_spec.rb
+++ spec/services/cloud/digital_ocean_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Cloud::DigitalOcean do
   describe '#initialize' do
-    it do
-      c = Cloud::DigitalOcean.new
-      binding.pry
+    before do
+      @c = Cloud::DigitalOcean.new
+    end
+    it 'instantiates from the new method' do
+      expect(@c).to be_kind_of(Cloud::DigitalOcean)
+    end
+    it 'exposes the droplet_kit client' do
+      expect(@c.client).to be_kind_of(DropletKit::Client)
     end
   end
 end
