be05d4ad0593bd52d919720a443e5964e9e98c54
Kingdon Barrett <kingdon.b@nd.edu>
2019-06-22 17:10:56 -0600

    respond to health checks at /healthz

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :temp_clusters do
     get 'kubeconfig'
   end
+  get '/healthz', to: proc { [200, {}, ['']] }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
